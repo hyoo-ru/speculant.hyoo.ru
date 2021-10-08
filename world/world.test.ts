e0100561f4d769c3a491bb6787fd2a2f0a816e7e
jin <jin@hyoo.ru>
2021-10-08 20:04:33 +0300

    Live indicators

--- world/world.test.ts
+++ world/world.test.ts
@@ -6,10 +6,25 @@ namespace $.$$ {
 			const world = new $hyoo_speculant_world
 			$mol_assert_equal( world.time().toString(), new $mol_time_moment().toString( 'YYYY-MM-DD' ) )
 			
-			world.time( !!'next' )
+			world.time( 'next' )
 			$mol_assert_unique( world.time().toString(), new $mol_time_moment().toString( 'YYYY-MM-DD' ) )
 			
-		}
+			world.destructor()
+		},
+		
+		'indicators'( $ ) {
+			
+			const world = new $hyoo_speculant_world
+			$mol_assert_equal( world.indicators().CSH.current, 1 )
+			$mol_assert_equal( world.indicators().KBK.history.length, 1 )
+			
+			world.time( 'next' )
+			$mol_assert_equal( world.indicators().CSH.current, 1 )
+			$mol_assert_equal( world.indicators().KBK.history.length, 2 )
+			$mol_assert_equal( world.indicators().KBK.history.slice(-1)[0], world.indicators().KBK.current )
+			
+			world.destructor()
+		},
 		
 	})
 }
--- world/world.view.ts
+++ world/world.view.ts
@@ -1,8 +1,16 @@
 namespace $.$$ {
+	
+	export type $hyoo_speculant_world_indicator = {
+		name: string
+		type: string
+		current: number
+		history: readonly number[]
+	}
+	
 	export class $hyoo_speculant_world extends $.$hyoo_speculant_world {
 		
 		@ $mol_mem
-		time( reset?: boolean ): $mol_time_moment {
+		time( next?: 'next' ): $mol_time_moment {
 			
 			this.$.$mol_state_time.now( 1000 )
 			
@@ -10,6 +18,29 @@ namespace $.$$ {
 			if( !prev ) return new $mol_time_moment().mask( '0000-00-00' )
 			
 			return prev.shift({ day: +1 })
+		}
+		
+		@ $mol_mem
+		indicators(): Record< 'CSH'|'KBK'|'BRT'|'RIK', $hyoo_speculant_world_indicator > {
+			
+			this.time()
+			
+			const prev = $mol_mem_cached( ()=> this.indicators() ) ?? super.indicators()
+			
+			for( const code in prev ) {
+				
+				const current = prev[ code ].current + Math.floor( Math.random() * 2 - 1 )
+				const history = [ ... prev[ code ].history, current ]
+				
+				prev[ code ] = {
+					... prev[ code ],
+					current,
+					history,
+				}
+				
+			}
+			
+			return { ... prev }
 		}
 		
 	}
