f4fc03f8f906ab05a65a11c0a68ca8bd7eabd7cf
PavelZubkov <zubkov_p@mail.ru>
2021-10-09 10:35:39 +0300

    Move balance to mol_labeler

--- app/dashboard/dashboard.view.css.ts
+++ app/dashboard/dashboard.view.css.ts
@@ -3,10 +3,7 @@ namespace $.$$ {
 	$mol_style_define( $hyoo_speculant_app_dashboard , {
 
 		Balance_total: {
-			fontSize: '2rem' ,
-			justifyContent: 'center' ,
-			padding: $mol_gap.block ,
-			paddingBottom: '0px' ,
+			fontWeight: 'bold' ,
 		}
 
 	} )
