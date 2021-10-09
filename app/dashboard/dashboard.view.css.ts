dbb56fa2c8b0ecf485de7ff122f277be15dd325b
PavelZubkov <zubkov_p@mail.ru>
2021-10-09 12:05:35 +0300

    Add paddings to legend items

--- app/dashboard/dashboard.view.css.ts
+++ app/dashboard/dashboard.view.css.ts
@@ -9,6 +9,28 @@ namespace $.$$ {
 		Head: {
 			justifyContent: 'space-between' ,
 		} ,
+		
+		Chart: {
+			Legend: {
+
+				Graph_sample_box: {
+					padding: $mol_gap.text ,
+				} ,
+
+				Graph_title: {
+					padding: $mol_gap.text ,
+				} ,
+				
+			} ,
+		} ,
+		
+		Rate: {
+			padding: $mol_gap.text ,
+		} ,
+
+		Balance_currency: {
+			padding: $mol_gap.text ,
+		} ,
 
 	} )
 
