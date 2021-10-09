753e5c448368213172be8267d6adbccd9f533e18
PavelZubkov <zubkov_p@mail.ru>
2021-10-09 07:55:34 +0300

    Improve legend styles

---
+++ chart/legend/legend.view.css.ts
@@ -0,0 +1,17 @@
+namespace $.$$ {
+
+	$mol_style_define( $hyoo_speculant_chart_legend , {
+
+		Graph_legend: {
+			whiteSpace: 'nowrap' ,
+			flex: {
+				grow: 0 ,
+				shrink: 0 ,
+				basis: 'auto' ,
+			} ,
+			alignItems: 'center' ,
+		} ,
+
+	} )
+
+}
