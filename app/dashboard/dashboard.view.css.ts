bdb338cb9066720e99b3909d34c83d36f7c1c98e
jin <jin@hyoo.ru>
2021-10-09 12:03:10 +0300

    Chat, themes, page sizes

--- app/dashboard/dashboard.view.css.ts
+++ app/dashboard/dashboard.view.css.ts
@@ -1,6 +1,12 @@
 namespace $.$$ {
+	
+	const { rem } = $mol_style_unit
 
 	$mol_style_define( $hyoo_speculant_app_dashboard , {
+		
+		flex: {
+			basis: rem(30),
+		},
 
 		Balance_total: {
 			fontWeight: 'bold' ,
