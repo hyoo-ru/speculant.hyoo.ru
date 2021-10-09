2b5015bfe0ea99519a28288825d1ebe38e20de52
PavelZubkov <zubkov_p@mail.ru>
2021-10-09 14:00:21 +0300

    Extract currency control from legend. Remove chart

--- app/dashboard/dashboard.view.ts
+++ app/dashboard/dashboard.view.ts
@@ -8,70 +8,40 @@ namespace $.$$ {
 		}
 		
 		@ $mol_mem
-		currency_chart() {
+		currency_work() {
 			return this.currency_all().filter( key => key !== 'CSH' ) as $hyoo_speculant_world_indicator_codes[]
 		}
 		
-		@ $mol_mem
-		day_start() {
-			return new this.$.$mol_time_moment
-		}
-		
-		@ $mol_mem
-		history_length() {
-			return this.indicator( this.currency_chart()[0] ).history.length
-		}
-		
-		@ $mol_mem
-		days() {
-			const arr = new Array( this.history_length() ).fill( '' )
-			return arr.map( ( _ , index ) => this.day_start().shift( { day: index } ).toString( 'MM-DD' ) )
-		}
-
 		@ $mol_mem_key
 		indicator( id : string ) {
 			return this.model().indicators()[ id ] as $hyoo_speculant_world_indicator
 		}
 
 		@ $mol_mem_key
-		linear_title( id : string ) {
+		currency_name( id : string ) {
 			return this.indicator( id ).name
 		}	
 		
-		@ $mol_mem_key
-		linear_series( id : string ) {
-			return this.indicator( id ).history
+		buy( id: $hyoo_speculant_world_indicator_codes ) { 
+			this.model().exchange( id , 1 )
 		}
 		
-		@ $mol_mem
-		linear_list() {
-			return [
-				... this.currency_chart().map( id => this.Linear( id ) ) ,
-				... super.linear_list() ,
-
-			]
-		}
-		
-		buy( index: number ) { 
-			this.model().exchange( this.currency_chart()[ index ] , 1 )
-		}
-		
-		sell( index: number ) {
-			this.model().exchange( this.currency_chart()[ index ] , -1 )
+		sell( id: $hyoo_speculant_world_indicator_codes ) {
+			this.model().exchange( id , -1 )
 		}
 
 		@ $mol_mem_key
-		currency_have( index: number ) {
-			return this.indicator( this.currency_chart()[ index ] ).have.toString()
+		currency_have( id: $hyoo_speculant_world_indicator_codes ) {
+			return this.indicator( id ).have.toString()
 		}
 		
 		@ $mol_mem_key
-		rate( index: number ) {
-			return this.indicator( this.currency_chart()[ index ] ).current.toString()
+		rate( id: $hyoo_speculant_world_indicator_codes ) {
+			return this.indicator( id ).current.toString()
 		}
 		
 		balance_total() {
-			const list = this.currency_chart().map( id => this.indicator( id ) )
+			const list = this.currency_work().map( id => this.indicator( id ) )
 			const sum = list.reduce( ( sum , cur ) => {
 				return sum + ( cur.current * cur.have )
 			} , 0 )
@@ -88,6 +58,12 @@ namespace $.$$ {
 		balance_cash() {
 			return this.indicator( 'CSH' ).have.toString()
 		}
+		
+		@ $mol_mem
+		currency_list() {
+			return this.currency_work().map( id => this.Row( id ) )
+		}
+
 	}
 
 }
